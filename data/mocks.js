21e5f16678e6fe7eef655a388d41972bd80939ae
Otto Giron <ottog2486@gmail.com>
2018-05-18 16:17:22 -0600

    mock some data for  Todo and todo Items

--- data/mocks.js
+++ data/mocks.js
@@ -1,5 +1,23 @@
+import { Query } from "mongoose";
+import casual from "casual";
+
 const mocks = {
-  String: () => 'It works!'
+  Query: () => ({
+    todo: (root, args) => {
+      return {id: args.id};
+    }
+  }),
+  Todo: () => ({
+    id: () => casual.integer(1,100),
+    version: () => casual.integer(1,10),
+    createdAt: () => casual.date(),
+    updatedAt: () => casual.date(),
+  }),
+  Item: () => ({
+    id: () => casual.integer(1,100),
+    desc: () => casual.description,
+    completed: () => casual.boolean,
+  })
 };
 
 export default mocks;
