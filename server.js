52542f74720d0bca6170196269c849b20770f355
Otto Giron <ottog2486@gmail.com>
2018-05-18 17:26:03 -0600

    Enable apollo engine

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
+import compression from 'compression';
+import { ApolloEngine } from 'apollo-engine';
+
 
 const GRAPHQL_PORT = 3000;
 
+
+const engine = new ApolloEngine({
+  apiKey: process.env.ENGINE_API_KEY,
+});
+
+
+
+
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+// Instead of app.listen():
+engine.listen({
+  port: GRAPHQL_PORT,
+  graphqlPaths: ['/graphql'],
+  expressApp: graphQLServer,
+  launcherOptions: {
+    startupTimeout: 3000,
+  },
+}, () => {
+  console.log('Listening!');
+});
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-  )
-);
+
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema, tracing: true}));
+graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+graphQLServer.use(compression());
